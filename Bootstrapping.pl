34a7bb1c85619d6eaffae8a3ecfa6926742c5d89
swati kaushik <swati.kaushik15@gmail.com>
2016-10-31 14:53:43 -0700

    First commit

---
+++ Bootstrapping.pl
@@ -0,0 +1,120 @@
+# perform t.test of wild type and mutant samples. Input data (gene expression matrix) should be sorted according to mutation information (wildtype to mutant as 0/1)
+# Keeps 10% of wild and mutant samples from dataset and perform bootstrapping to get difference in mean expression values
+# 2016-02-16
+
+use strict;
+use warnings;
+use Statistics::R;
+
+
+open GE, "non-redundant-LUADmatrix" or die $!;
+
+my @exprfile =<GE>;
+shift @exprfile;
+
+open OUT, ">>muliple-test-bootstrapping" or die $!;
+
+foreach (@exprfile){
+
+	my @rows = split("\t", $_);
+	my $geneid = shift @rows;
+	my $R = Statistics::R->new();
+	$R->set( 'x', \@rows);
+	$R->run( 'a <- t.test(x[2:527],x[528:577])[c("p.value","estimate")]');
+	my $squaresn = $R->get('a');
+	my @trainn = @$squaresn;
+	print OUT "$geneid\t$trainn[2]\t$trainn[10]\t$trainn[11]\t";
+	
+
+	my $n=0; my @train_pvalue; my @test_pvalue; my @train_w_mean; my @train_m_mean;  my @test_w_mean; my @test_m_mean;
+
+	while ($n <100)
+	{
+		#random number generation for wild type samples 
+		my $maxwild = 525; 	my $minwild = 0; my @randomwild; my @randomwild_GE_training; my %wildhash;my @randomwild_GE_test; 
+		
+		for (my $i=0; $i<53; $i++){
+			
+			#my $random_number_wild = int(rand($maxwild)) + $minwild;
+			my $random_number_wild = int($minwild + rand($maxwild - $minwild));
+			$wildhash{$random_number_wild}=$rows[$random_number_wild];
+			#print OUT "$rows[$random_number_wild]\t";
+			push (@randomwild_GE_test, $rows[$random_number_wild]);
+		
+		}
+		
+		for (my $i=0; $i<525; $i++){
+			
+			if (not exists ($wildhash{$i}))
+				{	push (@randomwild_GE_training, $rows[$i]);
+					#print OUT "$i\t$rows[$i]\n";
+				}
+		
+		}
+		
+		#random number generation for mutant samples 
+		my $maxmutant = 575; my $minmutant = 526;	my @randommutant; my @randommutant_GE_training; my %mutanthash; my@randommutant_GE_test;
+		
+		for (my $i=0; $i<5; $i++){
+			
+			my $random_number_mutant = int($minmutant + rand($maxmutant - $minmutant));
+			#print "$random_number_mutant\n";
+			if (!(exists($mutanthash{$random_number_mutant})))
+				{
+					$mutanthash{$random_number_mutant}=$rows[$random_number_mutant];
+					push (@randommutant_GE_test, $rows[$random_number_mutant]);
+					#print OUT "**$rows[$random_number_mutant]\t";
+				}
+		}		
+		
+		for (my $i=526; $i<575; $i++){
+			
+			if (not exists ($mutanthash{$i}))
+				{	push (@randommutant_GE_training, $rows[$i]);
+					#print OUT "$i\t$rows[$i]\n";
+				}
+		
+		}
+
+	# t.test of training set using R
+
+	my $R = Statistics::R->new();
+	$R->set( 'x', \@randomwild_GE_training);
+	$R->set( 'y', \@randommutant_GE_training);
+	$R->run( 'a <- t.test(x,y)[c("p.value","estimate")]');
+	my $squares = $R->get('a');
+	my @train = @$squares;
+	#print "$train[2]\t$train[10]\t$train[11]\t";
+	push (@train_pvalue, $train[2]); push (@train_w_mean, $train[10]); push (@train_m_mean, $train[11]);
+	
+	# t.test of test set using R
+
+	$R->set( 'x1', \@randomwild_GE_test);
+	$R->set( 'y1', \@randommutant_GE_test);
+	$R->run( 'a1 <- t.test(x1,y1)[c("p.value","estimate")]');
+	my $squares1 = $R->get('a1');
+	my @train1 = @$squares1;
+	#print "$train1[2]\t$train1[10]\t$train1[11]\n";
+	push (@test_pvalue, $train1[2]); push (@test_w_mean, $train1[10]); push (@test_m_mean, $train1[11]);
+
+	$n++;	
+	}
+	
+print OUT average(@train_pvalue), "\t";
+print OUT average(@train_w_mean), "\t";
+print OUT average(@train_m_mean), "\t";
+print OUT average(@test_pvalue), "\t";
+print OUT average(@test_w_mean), "\t";
+print OUT average(@test_m_mean), "\n";
+
+}
+
+#sub routine to calculate average of pvalue and means
+
+sub average {
+
+	my @array =@_;
+	my $sum;
+	foreach (@array) {$sum+=$_;}
+	return $sum/@array;
+}
---
+++ Mutual_exclusive_genes.pl
@@ -0,0 +1,149 @@
+# Calculate mutual exclusive interactors
+# Follows the same approach as TCGA mutual exclusivity analyses
+# March 21 2016
+
+use strict;
+use warnings;
+use Statistics::R;
+use List::MoreUtils qw(uniq);
+
+open IN, "direct-interactors"  or die $!;
+my @interactor_file = <IN>;
+
+my $gene_name = 'XX';
+
+open AH, "data_file.maf" or die $!;
+my @tcga_mutation_file =<AH>;
+
+open OUTALL, ">Odds-ratio-ints.txt" or die $!;
+
+foreach my $interactor_name (@interactor_file){
+	
+	print OUTALL "\n";	
+	$interactor_name =~s/\n|\t|\s//g;
+	print OUTALL "$interactor_name\t";
+	print "$interactor_name\t";
+	
+	my %XX_mutated_samples; my %interactor_mutated_sample; my %samples_not_altered_G1_G2; my %gene_sample_pairs; my %unique_samples;
+	
+
+	#open OUTE, ">XX_mutated_samples" or die $!;
+	#open OUTG, ">G2_mutated_samples" or die $!;
+	#open OUTB, ">samples_NOT_G1_G2_mutated_samples" or die $!;
+	#open OUTC, ">common_mutated_samples" or die $!;
+
+	foreach (@tcga_mutation_file){
+
+		my @mutation_rows = split("\t", $_);
+	
+		if (($mutation_rows[0] eq $gene_name) && ($mutation_rows[8] ne "Nonsense_Mutation") && ($mutation_rows[8] ne "Silent"))
+			{	
+				$mutation_rows[15] = substr($mutation_rows[15], 0,15);	
+				#print OUTE "$mutation_rows[0]\t$mutation_rows[15]\n";
+				$XX_mutated_samples{$mutation_rows[15]}=1;
+			}	
+
+		if (($mutation_rows[0] eq $interactor_name) && ($mutation_rows[8] ne "Nonsense_Mutation") && ($mutation_rows[8] ne "Silent"))
+			{	
+				$mutation_rows[15] = substr($mutation_rows[15], 0,15);	
+				#print OUTG "$mutation_rows[0]\t$mutation_rows[15]\n";
+				$interactor_mutated_sample{$mutation_rows[15]}=1;
+			}	
+		
+		if (($mutation_rows[8] ne "Nonsense_Mutation") && ($mutation_rows[8] ne "Silent"))
+			{	
+				$mutation_rows[15] = substr($mutation_rows[15], 0,15);	
+				$gene_sample_pairs{$mutation_rows[0]}{$mutation_rows[15]} =0;
+				$unique_samples{$mutation_rows[15]} =0;
+			}
+
+	}
+
+	#find samples which have both XX and interactor mutations. Compare XX and interactor mutation hash
+	my @common_samples=();
+
+	foreach (keys %XX_mutated_samples){
+
+		push (@common_samples, $_) if exists $interactor_mutated_sample{$_};
+	
+	}
+
+	#print OUTC join("\t",@common_samples),"\n";
+	my $common_mutated_samples = scalar@common_samples;
+	print OUTALL "$common_mutated_samples\t";
+
+	#Print XX mutated samples
+	foreach (keys %XX_mutated_samples){
+	
+		#print OUTE "$_\n";
+	}
+	my @XX_mutated_samples = keys %XX_mutated_samples;
+	my $XX_mutated_samples = scalar@XX_mutated_samples;
+	print OUTALL "$XX_mutated_samples\t";
+
+	#Print interactor mutated samples
+	foreach (keys %interactor_mutated_sample){
+	
+		#print OUTG "$_\n";
+	}
+	my @interactor_mutated_samples = keys %interactor_mutated_sample;
+	my $interactor_mutated_sample = scalar@interactor_mutated_samples;
+	print OUTALL "$interactor_mutated_sample\t"; 
+
+
+	#Print samples with no G1 or G2 mutations 
+	my @count;
+	foreach (keys %unique_samples){
+		
+		$_=~s/\n|\t|\s//g;
+		push (@count, $_) if exists $gene_sample_pairs{$gene_name}{$_};
+		push (@count, $_) if exists $gene_sample_pairs{$interactor_name}{$_};
+	
+	}		
+	my @mutated_unique_list = uniq @count;
+	my $mutated_unique_list = scalar @mutated_unique_list;
+	
+	my $total_samples = keys %unique_samples;
+	my $cases_altered_in_nethier_genes = $total_samples-$mutated_unique_list;
+	print OUTALL "$cases_altered_in_nethier_genes\t";
+	#print OUTALL scalar@mutated_unique_list,"\n";	
+	
+	
+	#Calculate odds ratio
+
+	my $cases_altered_in_XX_not_G2 = $XX_mutated_samples-$common_mutated_samples;
+	my $cases_altered_in_G2_not_XX = $interactor_mutated_sample-$common_mutated_samples;
+	
+	next if (($common_mutated_samples == 0) || ($cases_altered_in_nethier_genes == 0) || ($cases_altered_in_XX_not_G2 == 0)|| ($cases_altered_in_G2_not_XX == 0));
+			
+	my $odds_ratio = (($common_mutated_samples* $cases_altered_in_nethier_genes)/($cases_altered_in_XX_not_G2*$cases_altered_in_G2_not_XX));
+	my $odds_ratio_rounded =sprintf("%.3f",$odds_ratio);
+	print OUTALL "$odds_ratio_rounded\t";
+	my $log_odds_ratio_rounded = sprintf("%.3f",log10($odds_ratio));
+	print OUTALL "$log_odds_ratio_rounded\t";
+
+	#Calculate p-value on ODds ratio using Fischer test
+
+	my $R = Statistics::R->new();
+	$R->set( 'x', $common_mutated_samples);
+	$R->set( 'y', $cases_altered_in_nethier_genes);
+	$R->set( 'z', $cases_altered_in_XX_not_G2);
+	$R->set( 'w', $cases_altered_in_G2_not_XX);
+	$R->run('tab <-matrix(c(x,z,w,y), nrow=2)');
+	$R->run( 'a <- fisher.test(tab)');
+	my $squares = $R->get('a');
+	#my @train = @$squares;
+	my @fis = split("\n", $squares);
+	$fis[3]=~s/p-value =//g;
+	print OUTALL "$fis[3]\t$fis[9]";
+	
+	undef %XX_mutated_samples; undef %interactor_mutated_sample; undef %samples_not_altered_G1_G2; undef %unique_samples; undef %gene_sample_pairs;
+
+}
+
+#subroutine to calculate log10 of odds ratio
+sub log10 {
+	  my $n = shift;
+	  return log($n)/log(10);
+}
+
